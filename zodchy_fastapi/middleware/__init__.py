cafa2fa11b4a36cdbc4b48e3343ee8a301fa7ca1
Sergey Feofilaktov <s.feofilaktov@net-vision.pro>
2025-01-04 20:25:17 +0300

    - Added security middleware

--- zodchy_fastapi/middleware/__init__.py
+++ zodchy_fastapi/middleware/__init__.py
@@ -2,3 +2,16 @@ from .handlers import (
     generic_exception_handler,
     validation_exception_handler
 )
+from .security import (
+    JwtAuthMiddleware,
+    GatewayAuthMiddleware,
+    AuthMiddleware
+)
+
+__all__ = [
+    "JwtAuthMiddleware",
+    "GatewayAuthMiddleware",
+    "AuthMiddleware",
+    "generic_exception_handler",
+    "validation_exception_handler"
+]
